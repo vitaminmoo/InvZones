cb3900d014a2752efc421cae9383f85b00b56dbe
Graham Forest <vitaminmoo@wza.us>
2011-04-21 09:36:01 -0700

    finally worksish - wee

--- src/vitaminmoo/InvZones/InvZones.java
+++ src/vitaminmoo/InvZones/InvZones.java
@@ -1,11 +1,14 @@
 package vitaminmoo.InvZones;
 
-import java.util.HashMap;
+import java.util.logging.Logger;
 
-import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
 
 /**
  * InvZones for Bukkit
@@ -15,17 +18,41 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class InvZones extends JavaPlugin {
     private final InvZonesPlayerListener playerListener = new InvZonesPlayerListener(this);
 
+    private static String clearedMsg = "Your inventory has been cleared";
+
+    private static Logger log;
+    private Configuration config;
+
     public void onEnable() {
+
+        config = this.getConfiguration();
+        log = Logger.getLogger("Minecraft");
 
         // Register our events
         PluginManager pm = getServer().getPluginManager();
-       
+
         PluginDescriptionFile pdfFile = this.getDescription();
-        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+        log.info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+
+        this.reloadConfig();
+
+        pm.registerEvent(Event.Type.PLAYER_TELEPORT, playerListener, Priority.Normal, this);
+
     }
+
+    public void reloadConfig() {
+        config.load();
+        clearedMsg = config.getString("cleared-message", clearedMsg);
+        saveConfig();
+    }
+    public void saveConfig() {
+        config.setProperty("cleared-message", clearedMsg);
+        config.save();
+    }
+
     public void onDisable() {
         PluginDescriptionFile pdfFile = this.getDescription();
-        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is disabled" );
+        log.info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is disabled" );
     }
 }
 
--- src/vitaminmoo/InvZones/InvZonesPlayerListener.java
+++ src/vitaminmoo/InvZones/InvZonesPlayerListener.java
@@ -1,6 +1,12 @@
 package vitaminmoo.InvZones;
 
+import java.util.logging.Logger;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 /**
  * Handle events for all Player related events
@@ -12,5 +18,12 @@ public class InvZonesPlayerListener extends PlayerListener {
     public InvZonesPlayerListener(InvZones instance) {
         plugin = instance;
     }
+
+    @Override
+        public void onPlayerTeleport(PlayerTeleportEvent event) {
+            Logger log = Logger.getLogger("Minecraft");
+            Player player = event.getPlayer();
+            log.info(player.getName() + " teleported from " + event.getFrom().getWorld().getName() + " to " + event.getTo().getWorld().getName());
+        }
 }
 
